c0aff410a9de3bc89ddc8d655198f6b4503ee789
Eason chiu <76670758@qq.com>
2017-07-13 11:24:29 +0800

    fix

--- generators/app/templates/src/views/demo/index.jsx
+++ generators/app/templates/src/views/demo/index.jsx
@@ -3,12 +3,7 @@ import React, { Component } from 'react'
 import connect from 'src/redux/connectProps'
 import mass from 'src/assets/libs/mass'
 
-
-
 import List from 'src/containers/list'
-
-import {Layout,Cell,Button} from 'src/auto'
-
 
 
 class ViewDemo extends Component {
@@ -24,8 +19,9 @@ class ViewDemo extends Component {
 			button: 'btnB',
 			click: this.buttonClick,
 		}]
-
+		
 	}
+
 	buttonClick(target) {
 		if (target === 'async btnA') {
 			this.props.$demo.anyncEmit({
@@ -37,49 +33,22 @@ class ViewDemo extends Component {
 			})
 		}
 	}
-	_click(id){
-		
-	}
+	
 	render() {
 		return (
-			<Layout>
-				<Layout.Header />
-
-				
-				<Layout.Body>
-					
-					<Cell onClick={::this._click}>
-						<Cell.Row key={1} value="a">
-							<Button>Button</Button>
-							<hr />
-							<Button disabled>Button</Button>
-							<hr />
-							<Button ghost>Button</Button>
-							<hr />
-							<Button ghost disabled>Button</Button>
-						</Cell.Row>
-						<Cell.Row key={2} value="b">
-							Cell
-						</Cell.Row>
-					</Cell>
-
-				</Layout.Body>
-
-
-				<Layout.Footer />
-			</Layout>
+			<fieldset styleName="view-demo">
+				<legend>I am 'demo' view</legend>
+				<h1 styleName="title">demo view with react</h1>
+				<p styleName="info">store: { this.props.$$demo.name }</p>
+				<List content={ this.list } />
+				<br />
+				<List content={ this.list } />
+			</fieldset>
 		)
 	}
 }
 
 
-// <fieldset styleName="view-demo">
-// 				<legend>I am 'demo' view</legend>
-// 				<h1 styleName="title">demo view with react</h1>
-// 				<p styleName="info">store: { this.props.$$demo.name }</p>
-// 				<List content={ this.list } />
-// 				<br />
-// 				<List content={ this.list } />
-// 			</fieldset>
+
 
 export default connect(mass(ViewDemo, styles))
